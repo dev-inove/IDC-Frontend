fa92c3c6b0534f2d158b6e14dc5d2454c8bfa099
Gustavo Fernandes <63157775+gufernandess@users.noreply.github.com>
2023-06-20 14:40:48 -0300

    feat - Implementação final da tela de login (Validações, refatoração e rotas) (#2)

---
+++ src/pages/Login/ResetPassword/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap'
+import '../styles.css'
+
+const ResetPassword = () => {
+    return(
+        <Container fluid className="container p-0 m-0">
+            <Container className="container logo p-0 m-0"></Container>
+            <Container className="container login p-0 m-0"></Container>
+        </Container>
+    );
+};
+
+export default ResetPassword;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import LoginPage from '../pages/LoginPage';
+import Login from '../pages/Login';
+import ResetPassword from '../pages/Login/ResetPassword/index';
+import Home from '../pages/Home';
 
 const routes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<LoginPage />} />
+      <Route path='/' element={<Login />} />
+      <Route path='/resetPassword' element={<ResetPassword />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
